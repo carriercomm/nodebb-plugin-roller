95049c0bef091296d497e24f8aed08a477c03f22
Connor Hillen <connor.hillen@gmail.com>
2015-03-06 14:24:20 -0500

    Updated library to include logs

--- library.js
+++ library.js
@@ -9,6 +9,9 @@
   // A function to scrape the roll for the keys
   // Returns the randomized value
   var generateRoll = function(str) {
+
+    console.log("Generating roll...");
+
     var total   = 0
       , matched = diceRegex.exec(str)
       , numDice = matched[1]
@@ -19,24 +22,26 @@
     // If there is a diceMod, add it. Parsing the statement will remove the + or
     // convert the - into a negative integer.
     if (diceMod)
-      total += parseInt(diceMod)
+      total += parseInt(diceMod);
 
     // Loop through numDice times and constantly add the values of the dice
     // each time, between 1 and diceVal
     for(var i = 0; i < numDice; i++ )
       total += Math.floor((Math.random() * diceVal) + 1);
 
+    console.log("Roll generated: " + total);
     // Give it a special class, so that it can be styled to show it's a roll
     return "<span title='"+matched[0]+"' class='diceRoll'>"+total+"</span>";
   }
 
   Roller.parse = function(data, callback) {
+    console.log("Roll Parser Loaded.");
+
     if (!data || !data.postData || !data.postData.content) {
       return callback(null, data);
     }
 
     if (data.postData.content.match(diceRegex)) {
-
       data.postData.content =
         data.postData.content.replace(diceRegex,
                                       generateRoll(data.postData.content));
