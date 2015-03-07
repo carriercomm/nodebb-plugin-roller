5e64b2812d4335a18a60f6f554f173b8c2716cff
Connor Hillen <connor.hillen@gmail.com>
2015-03-07 14:40:11 -0500

    Updated to store the roll in the database

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
-(function(module) {
-  "use strict";
+var posts = require('../../src/posts');
 
+(function(module) {
   // Dice roller formats
   // /roll #d# (+|-) #
   // 1: Number of dice, 2, dice value, 3, +/- something
@@ -12,9 +12,6 @@
   // A function to scrape the roll for the keys
   // Returns the randomized value
   var generateRoll = function(str) {
-
-    console.log("Generating roll...");
-
     var total   = 0
       , matched = diceRegex.exec(str)
       , numDice = matched[1]
@@ -32,24 +29,25 @@
     for(var i = 0; i < numDice; i++ )
       total += Math.floor((Math.random() * diceVal) + 1);
 
-    console.log("Roll generated: " + total);
     // Give it a special class, so that it can be styled to show it's a roll
     return "<span title='"+matched[0]+"' class='diceRoll'>"+total+"</span>";
   }
 
   Roller.parse = function(data, callback) {
-    console.log("Roll Parser Loaded.");
-
     if (!data || !data.postData || !data.postData.content) {
       return callback(null, data);
     }
 
     if (data.postData.content.match(diceRegex)) {
-      data.postData.content =
-        data.postData.content.replace(diceRegex,
-                                      generateRoll(data.postData.content));
+      if (!data.postData.roll) {
+        var roll = generateRoll(data.postData.content);
+        posts.setPostField(data.postData.pid, "roll", roll, function() {});
+        data.postData.content = data.postData.content.replace(diceRegex, roll);
       }
-
+      else {
+        data.postData.content = data.postData.content.replace(diceRegex, data.postData.roll);
+      }
+    }
       callback(null, data);
     };
 
